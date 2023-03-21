52726784e93efa3b56af1d0708300952c75724b3
unknown <iamdeveshd11@gmail.com>
2023-03-21 12:27:19 +0530

    first commit

---
+++ Database Design/AdvancedBankingSystem_mysql_create.sql
@@ -0,0 +1,50 @@
+CREATE TABLE `User` (
+	`id` varchar(50) NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Executive` (
+	`id` bigint NOT NULL AUTO_INCREMENT,
+	`password` varchar(50) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Counter` (
+	`id` bigint NOT NULL AUTO_INCREMENT,
+	`cNumber` bigint NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Service` (
+	`id` bigint NOT NULL AUTO_INCREMENT,
+	`serviceName` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `m2m` (
+	`executiveID` bigint NOT NULL,
+	`serviceID` bigint NOT NULL
+);
+
+CREATE TABLE `Token` (
+	`number` bigint NOT NULL AUTO_INCREMENT,
+	`serviceId` bigint NOT NULL,
+	`counterId` bigint,
+	PRIMARY KEY (`number`)
+);
+
+ALTER TABLE `m2m` ADD CONSTRAINT `m2m_fk0` FOREIGN KEY (`executiveID`) REFERENCES `Executive`(`id`);
+
+ALTER TABLE `m2m` ADD CONSTRAINT `m2m_fk1` FOREIGN KEY (`serviceID`) REFERENCES `Service`(`id`);
+
+ALTER TABLE `Token` ADD CONSTRAINT `Token_fk0` FOREIGN KEY (`serviceId`) REFERENCES `Service`(`id`);
+
+ALTER TABLE `Token` ADD CONSTRAINT `Token_fk1` FOREIGN KEY (`counterId`) REFERENCES `Counter`(`id`);
+
+
+
+
+
+
+
